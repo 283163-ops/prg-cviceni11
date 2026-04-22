eaca8f567c178df7fd14a0884fb5e0493eab4011
Tereza Filipová <283163@vutbr.cz>
2026-04-22 10:35:33 +0200

    studentgrades

---
+++ studentgrades.py
@@ -0,0 +1,44 @@
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores) #zjistuje pocet studentu
+
+    def get_grade(self, index): # dam si novou definici metody
+        score = self.get_by_index(index)
+        if score < 50:
+            return "F"
+        elif 50 <= score < 60:
+            return "E"
+        elif 60 <= score < 70:
+            return "D"
+        elif 70 <= score < 80:
+            return "C"
+        elif 80 <= score < 90:
+            return "B"
+        elif 90 <= score <= 100:
+            return "A"
+
+    def find(self, score):
+        idx = 0
+        count = 0
+        positions = []
+        searched_data = self.scores
+        while idx < len(searched_data):
+            if searched_data == score:
+                positions.append(idx)
+            idx += 1
+        return positions
+
+
+if __name__ == "__main__":
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    print(results.count()) # 9 pocet student
+    print(results.get_by_index(2))  #treti student ma 91 bodu
+    print(results.scores)
+    print(results.get_grade(5))# vypsani vsech skor
+    print(results.find(4))
